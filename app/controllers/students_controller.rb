c225220208986954c3ba8e6a50fca43f113baba9
Dilshan <dilshan.mudali@gmail.com>
2021-11-01 20:37:43 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.where('first_name = ?', params[:name]).or(Student.where('last_name = ?', params[:name]))
     render json: students
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
